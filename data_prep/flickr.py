ce4c44c3d7e3392f2ed4a68882461176da87f04b
RussellSB <r.sammutbonnici@gmail.com>
2021-07-01 15:31:31 +0000

    Added arg functionality

--- data_prep/flickr.py
+++ data_prep/flickr.py
@@ -1,13 +1,17 @@
 from tqdm import tqdm
 import numpy as np
+import argparse
 import shutil
 import os
 
-dataroot = '../../datasets/flickr_audio/flickr_audio/'
-outdir = '../data/data_flickr/'
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataroot", type=str, default='../../datasets/flickr_audio/flickr_audio/', help="data root of flickr")
+parser.add_argument("--outdir", type=str, default='../voice_conversion/data/data_flickr/', help="output directory")
+opt = parser.parse_args()
+print(opt)
 
 # This just gets the filenames with speaker referenes (because it is not clear in the filename otherwise)
-textin = dataroot + 'wav2spk.txt'
+textin = opt.dataroot + 'wav2spk.txt'
 speaker_files = np.genfromtxt(textin, dtype=[('mystring','S27'),('myint','i8')], delimiter=' ')
 
 # Saves wavs belonging to speaker from list of speaker files
@@ -16,9 +20,9 @@ def flickr_prep_wavs(outdir, speaker_files, src):
     
     files = [filename for (filename, spk) in speaker_files if spk == src]
     for filename in tqdm(files, desc="extracting spk %s"%src):
-        f = dataroot + 'wavs/' + filename.decode()   
+        f = opt.dataroot + 'wavs/' + filename.decode()   
         shutil.copy(f, outdir)
 
 # Preperation for preprocessing from VAEGAN repo
-flickr_prep_wavs(outdir+'spkr_1', speaker_files, 4)
-flickr_prep_wavs(outdir+'spkr_2', speaker_files, 7)
+flickr_prep_wavs(opt.outdir+'spkr_1', speaker_files, 4)
+flickr_prep_wavs(opt.outdir+'spkr_2', speaker_files, 7)
--- data_prep/urmp.py
+++ data_prep/urmp.py
@@ -1,14 +1,18 @@
 from tqdm import tqdm
+import argparse
 import shutil
 import glob
 import os
 
-dataroot = '../../datasets/urmp/'
-outdir = '../data/data_urmp/'
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataroot", type=str, default='../../datasets/urmp/', help="data root of flickr")
+parser.add_argument("--outdir", type=str, default='../voice_conversion/data/data_urmp/', help="output directory")
+opt = parser.parse_args()
+print(opt)
 
 # Gets the audio seperated wavs of a specific instrument from the URMP dataset
 def get_audiosep_ins(ins):
-    return glob.glob(dataroot + '**/AuSep*'+ins+'*.wav', recursive = True)
+    return glob.glob(opt.dataroot + '**/AuSep*'+ins+'*.wav', recursive = True)
 
 # Saves wavs belonging to speaker from list of speaker files
 def urmp_prep_wavs(outdir, instrument_files, src):
@@ -20,5 +24,5 @@ trumpet_files = get_audiosep_ins('tpt')
 violin_files = get_audiosep_ins('vn')
 
 # Preperation for preprocessing from VAEGAN repo
-urmp_prep_wavs(outdir+'spkr_1', trumpet_files, 'tpt')
-urmp_prep_wavs(outdir+'spkr_2', violin_files, 'vn')
+urmp_prep_wavs(opt.outdir+'spkr_1', trumpet_files, 'tpt')
+urmp_prep_wavs(opt.outdir+'spkr_2', violin_files, 'vn')
