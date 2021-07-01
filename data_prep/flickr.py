3a9a63acf7e1528cc5a202cc5695624ca780d30a
RussellSB <r.sammutbonnici@gmail.com>
2021-07-01 13:58:22 +0000

    Updated data prep to Ebadawy's way

--- data_prep/flickr.py
+++ data_prep/flickr.py
@@ -1,35 +1,23 @@
-import soundfile as sf
 from tqdm import tqdm
-from io import StringIO
 import numpy as np
-import librosa
 import shutil
 import os
 
-# Constants
-WAVPATH = '../../ebagan-voice-conversion/db_flickr/spkr_2' # the output directory
-SPK = 7
-MAX = 100000
+dataroot = '../../datasets/flickr_audio/flickr_audio/'
+outdir = '../data/data_flickr/'
 
 # This just gets the filenames with speaker referenes (because it is not clear in the filename otherwise)
-path = '../flickr_audio/flickr_audio/wav2spk.txt'
-s = StringIO(u"1,1.3,abcde")
-speaker_files = np.genfromtxt(path, dtype=[('mystring','S27'),('myint','i8')], delimiter=' ')
+textin = dataroot + 'wav2spk.txt'
+speaker_files = np.genfromtxt(textin, dtype=[('mystring','S27'),('myint','i8')], delimiter=' ')
 
-# Saves amnt samples belonging to speaker from list of speaker files
-def flickr_prep_wavs(outdir, speaker_files, src, max_amnt):
+# Saves wavs belonging to speaker from list of speaker files
+def flickr_prep_wavs(outdir, speaker_files, src):
+    os.makedirs(outdir, exist_ok=True)
     
-    print('Extracting and copying all source files to specified directory...')
-    if not os.path.exists(outdir): os.mkdir(outdir)
-    
-    amnt = 0
-    for filename, spk in speaker_files:
-        if amnt >= max_amnt: break
-        if spk == src: 
-            f = '../flickr_audio/flickr_audio/wavs/' + filename.decode()   
-            shutil.copy(f, WAVPATH)
-        amnt += 1
-            
-    print('Finished!')
-        
-flickr_prep_wavs(WAVPATH, speaker_files, SPK, MAX)
+    files = [filename for (filename, spk) in speaker_files if spk == src]
+    for filename in tqdm(files, desc="extracting spk %s"%src):
+        f = dataroot + 'wavs/' + filename.decode()   
+        shutil.copy(f, outdir)
+
+flickr_prep_wavs(outdir+'spkr_1', speaker_files, 4)
+flickr_prep_wavs(outdir+'spkr_2', speaker_files, 7)
--- data_prep/urmp.py
+++ data_prep/urmp.py
@@ -1,61 +1,23 @@
-import soundfile as sf
 from tqdm import tqdm
-import numpy as np
-import librosa
 import shutil
 import glob
 import os
 
-# Constants
-WAVPATH = '../../ebagan-voice-conversion/db_urmp/spkr_1' # the output directory
-INS = 'tpt'
-MAX = 100000
+dataroot = '../../datasets/urmp/'
+outdir = '../data/data_urmp/'
 
 # Gets the audio seperated wavs of a specific instrument from the URMP dataset
-# Can set ins as 'vc' or 'tpt'
 def get_audiosep_ins(ins):
-    return glob.glob('../urmp/**/AuSep*'+ins+'*.wav', recursive = True)
+    return glob.glob(dataroot + '**/AuSep*'+ins+'*.wav', recursive = True)
 
-# Returns subsamples from audiosample of frame length 128 (in the case of 16000 sr, 800 n_fft, 200 hop length) 
-# Accepts audio x, saves its subsamples to outdir
-def subsample_x(x, outdir, filename, goal_frames, hop, sr):
-
-        #ipd.display(ipd.Audio(x, rate=sr))
-        sub_samples = (goal_frames * hop) # the desired samples we want so they will have 128 frames each after processing
-        full_samples = len(x)
+# Saves wavs belonging to speaker from list of speaker files
+def urmp_prep_wavs(outdir, instrument_files, src):
+    os.makedirs(outdir, exist_ok=True)
+    for f in tqdm(instrument_files, desc="extracting ins %s"%src):
+        shutil.copy(f, outdir)
         
-        if full_samples >= sub_samples:
-            
-            for i, t in enumerate(range(0, full_samples, sub_samples)):
-                start = t
-                end = t + sub_samples
-                
-                # if out of bounds, pad with zeros
-                if end > full_samples: 
-                    sub_wav = np.concatenate([x[start:], np.zeros(end-full_samples-1)])
-                else: # if in bounds just extract normally
-                    sub_wav = x[start:end-1]
-                    
-                #if sub_wav.size != 25599:
-                #   print(sub_wav.size)
-                outfilename = outdir + filename + '_' + f"{i:03}" + '.wav'
-                sf.write(outfilename, sub_wav, sr) 
-                if end > full_samples: break
+trumpet_files = get_audiosep_ins('tpt')
+violin_files = get_audiosep_ins('vn')
 
-# Saves amnt samples belonging to speaker from list of speaker files
-# Also subsambled to 1.6s / 128 frames. But named in a way that it can later be concatenated
-def urmp_prep_wavs(outdir, instrument_files, src, max_amnt):
-    
-    print('Extracting and subdividing all source files to specified directory...')
-    if not os.path.exists(outdir): os.mkdir(outdir)
-    
-    amnt = 0
-    for f in instrument_files:
-        if amnt >= max_amnt: break
-        shutil.copy(f, WAVPATH)
-        amnt += 1
-            
-    print('Finished!')
-        
-instrument_files = get_audiosep_ins(INS)
-urmp_prep_wavs(WAVPATH, instrument_files, INS, MAX)
+urmp_prep_wavs(outdir+'spkr_1', trumpet_files, 'tpt')
+urmp_prep_wavs(outdir+'spkr_2', violin_files, 'vn')
