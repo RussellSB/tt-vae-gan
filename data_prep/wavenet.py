b8e7518e2d5c769dcb3b6c723dea15e47d2dac45
RussellSB <r.sammutbonnici@gmail.com>
2021-08-08 02:11:27 +0000

    Option for general vocoder training

--- data_prep/wavenet.py
+++ data_prep/wavenet.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, help="path to prepared dataset folder")
 parser.add_argument("--outdir", type=str, help="path to output directory")
 parser.add_argument("--tag", type=str, help="tag for datasource reference")
+parser.add_argument("--mode", type=int, default=0, help="0 for training generally on all domains. 1 for training specific to a domain.")
 args = parser.parse_args()
 print(args)
 
@@ -28,7 +29,12 @@ def to_wavenet(refs_spk, wavdir, outdir):
 # Data preparation
 num_spk = len(refs_train)
 for i in range(num_spk):
-    outdir = os.path.join(args.outdir, args.tag + '_%d'%(i+1))
+    
+    localdir = args.tag
+    if args.mode == 1:
+        localdir += '_%d'%(i+1)
+        
+    outdir = os.path.join(args.outdir, localdir)
     wavdir = os.path.join(args.dataset, 'spkr_%d'%(i+1))
     
     to_wavenet(refs_train[i], wavdir, os.path.join(outdir, 'train_no_dev'))
